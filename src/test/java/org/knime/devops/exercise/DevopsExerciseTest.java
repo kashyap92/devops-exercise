db500738a37a9ca7b2f8a10f064ddfbd9c326f8f
kashyap92 <kashyap022@yahoo.in>
2022-07-22 03:23:24 +0200

    change class name

--- src/test/java/org/knime/devops/exercise/DevopsExerciseTest.java
+++ src/test/java/org/knime/devops/exercise/DevopsExerciseTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class Testcases {
+public class DevopsExerciseTest {
 	@Test
 	void succeedingTest() {
 		// does nothing on purpose
